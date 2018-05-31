b4a8c3e6a183b6d3760524ac6f69dc299c982e2c
建新徐 <jianxinxu@jianxinxudeMacBook-Pro.local>
2018-05-31 13:02:54 +0800

    first commit

---
+++ Contract.js
@@ -0,0 +1,409 @@
+'use strict';
+
+const unit_nas=new BigNumber(1000000000000000000);
+
+
+var Issuer = function (obj) {
+    if (typeof obj === "string") {
+        obj = JSON.parse(obj);
+    }
+    if (typeof obj === "object") {
+        this.name = obj.name;
+    }
+    else {
+        this.name = "";
+    }
+}
+
+Issuer.prototype = {
+    toString: function () {
+        return JSON.stringify(this);
+    }
+}
+
+var DinnerList = function(obj) {
+    if (typeof obj === "string") {
+        this.ids = JSON.parse(obj);
+    }
+    else if(Object.prototype.toString.call(obj)=='[object Array]') {
+        this.ids=obj;
+    }
+    else
+        this.ids=[];
+}
+
+DinnerList.prototype = {
+    toString: function () {
+        return JSON.stringify(this.ids);
+    },
+    addDinner:function(id){
+        for(var i=0;i<this.ids.length;++i)
+            if(id == this.ids[i])
+                return;
+        this.ids.push(id);
+    },
+    removeDinner:function(id){
+        for(var i=0;i<this.ids.length;++i) {
+            if(id == this.ids[i]) {
+                this.ids.splice(i,1);
+                return;
+            }
+        }
+    }
+}
+
+var Dinner = function(obj) {
+    if (typeof obj === "string") {
+        obj = JSON.parse(obj);
+        for(var i=0;i<obj.bidders.length;++i)
+            obj.bidders[i].nasInWei = new BigNumber(obj.bidders[i].nasInWei);
+    }
+    if (typeof obj === "object") {
+        this.ownerAddr = obj.ownerAddr;     //发起人钱包地址
+        this.hash = obj.hash;               //发起交易的hash
+        this.title = obj.title;             //主题
+        this.text = obj.text;               //活动介绍(html格式)
+        this.beginBlock = obj.beginBlock;   //开始报名区块号
+        this.endBlock = obj.endBlock;       //结束报名区块号
+        this.nasTook = obj.nasTook;                 //nas是否已经领取
+        this.bidders = obj.bidders||[];             //竞投的历史记录
+        this.minBidStep = obj.minBidStep||"0.1";    //竞价起跳最多NAS数
+        this.maxBidStep = obj.maxBidStep||"5";      //竞价起跳最多NAS数
+        this.sharePercent = obj.sharePercent||"0";  //投标人分享投标比例（百分比）
+    }
+};
+
+Dinner.prototype = {
+    toString: function () {
+        return JSON.stringify(this);
+    },
+    bid:function(hash,addr,user,nasInWei,time){
+        if(!this.isActive()) {
+            return "not active";
+        }
+        var maxLimit = unit_nas.times(this.maxBidStep);
+        var minLimit = unit_nas.times(this.minBidStep);
+        var topBidder = this.getTopBidder();        
+        if(!!topBidder) {
+            if(nasInWei.lt(topBidder.nasInWei.plus(minLimit))) {
+                return "nas is too little";
+            }
+            if(nasInWei.eq(topBidder.nasInWei)) {
+                if(time >= topBidder.time)
+                    return "time is too late";
+            }
+            maxLimit=topBidder.nasInWei.plus(maxLimit);
+        }
+        if(nasInWei.gt(maxLimit)) {
+            return "nas is too more";
+        }
+
+        if(!!topBidder)
+            this.sendNasBack(topBidder);
+        
+        this.bidders.push({
+            hash:hash,
+            addr:addr,
+            user:user,
+            nasInWei:nasInWei.toString(10),
+            time:time
+        });
+        return "ok";
+    },
+    getTopBidder:function() {
+        if(this.bidders.length>0)
+            return this.bidders[this.bidders.length-1]
+        return null;
+    },
+    sendNasBack:function(bidder) {
+        if(!!bidder&&bidder.nasInWei.gt(0)) {
+            var result = Blockchain.transfer(bidder.addr, bidder.nasInWei);
+            console.log("transfer result:", result);
+            if(result) {
+                Event.Trigger("transfer", {
+                    Transfer: {
+                        from: Blockchain.transaction.to,
+                        to: bidder.addr,
+                        value: bidder.nasInWei
+                    }
+                });
+            }
+        }
+    },
+    isActive:function(){
+        var block = Blockchain.block.height;
+        return block>=this.beginBlock&&block<this.endBlock;
+    }
+}
+
+
+var DinnerContract = function () {
+    LocalContractStorage.defineProperties(this, {
+        _name: null,
+        _creator: null
+    });
+
+    LocalContractStorage.defineMapProperties(this, {
+        "dinners": {
+            parse: function (value) {
+                return new Dinner(value);
+            },
+            stringify: function (o) {
+                return o.toString();
+            }
+        },
+        "issuers": {
+            parse: function (value) {
+                return new Issuer(value);
+            },
+            stringify: function (o) {
+                return o.toString();;
+            }
+        },
+        "issuerDinnerList": {
+            parse: function (str) {
+                return new DinnerList(str);
+            },
+            stringify: function (o) {
+                return o.toString();
+            }
+        }
+    });
+};
+
+DinnerContract.prototype = {
+    //智能合约初始化函数，只会在部署的时候执行一次
+    init: function () {
+        this._name = "17Fan8";
+        this._creator = Blockchain.transaction.from;
+    },
+
+    name: function () {
+        return this._name;
+    },
+
+    //添加颁布地址，只有这些地址才能颁布奖状
+    addIssuerAddr:function (addr,name) {
+        var from = Blockchain.transaction.from;
+        if(from != this._creator) {
+            throw new Error("You are not the manager!");
+        }
+
+        this.issuers.set(addr,new Issuer({name:name}));
+    },
+
+    //删除颁布地址
+    removeIssuerAddr:function (addr) {
+        var from = Blockchain.transaction.from;
+        if(from != this._creator) {
+            throw new Error("You are not the manager!");
+        }
+
+        this.issuers.del(addr);
+    },
+
+    //添加活动
+    addDinner:function (ownerAddr, title, text, beginBlock, endBlock, minBidStep, maxBidStep, sharePercent) {
+        if(Blockchain.verifyAddress(ownerAddr)!==87) {
+            throw new Error("ownerAddr is not a user address!");
+        }
+
+        var from = Blockchain.transaction.from;
+        var issuer = this.issuers.get(from);
+        if(issuer == null) {
+            throw new Error("You are not a issuer!");
+        }
+
+        var hash = Blockchain.transaction.hash;
+        this.dinners.set(hash,new Dinner({
+            ownerAddr:ownerAddr,
+            hash:hash,
+            title:title,
+            text:text,
+            beginBlock:parseInt(beginBlock),
+            endBlock:parseInt(endBlock),
+            minBidStep:minBidStep,
+            maxBidStep:maxBidStep,
+            sharePercent:sharePercent
+        }));
+
+        var list = this.issuerDinnerList.get(from)||new DinnerList();
+        list.addDinner(hash);
+        this.issuerDinnerList.set(from,list);
+    },
+
+    //删除活动
+    removeDinner:function (hash) {
+        var from = Blockchain.transaction.from;
+        var dinner = this.dinners.get(hash);
+        if(!dinner) {
+            throw new Error("Can't find dinner!");
+        }
+        
+        if(from != dinner.ownerAddr && this._creator != from) {
+            throw new Error("You have not permission!");
+        }
+
+        this.dinners.del(hash);
+
+        var list = this.issuerDinnerList.get(from);
+        if(list) {
+            list.removeDinner(hash);
+            this.issuerDinnerList.set(from,list);
+        }
+    },
+
+    // 查询所有活动
+    listByIssuer: function (issuer) {
+        return this.issuerDinnerList.get(issuer);
+    },
+
+    info:function(hash) {
+        return this.dinners.get(hash);
+    },
+
+    //竞标
+    bid: function(hash,user) {
+        var dinner = this.dinners.get(hash);
+        if(!dinner) {
+            return "no dinner matched!";
+        }
+
+        var result = dinner.bid(Blockchain.transaction.hash,
+            Blockchain.transaction.from,
+            user,
+            Blockchain.transaction.value,
+            Blockchain.transaction.timestamp);
+        if(result!="ok")
+            return result;
+        this.dinners.set(hash,dinner);
+        return "ok";
+    },
+
+    //领取Nas
+    takeNasByOwner:function(hash) {
+        var dinner = this.dinners.get(hash);
+        if(!dinner) {
+            return "no dinner matched!";
+        }
+
+        if(dinner.isActive()) {
+            return "bid has not stopped!";
+        }
+
+        if(dinner.nasTook) {
+            return "the nas has be took."
+        }
+
+        var from = Blockchain.transaction.from;
+        if(dinner.ownerAddr !== from) {
+            return "not owner address!"
+        }
+
+        dinner.nasTook=true;
+        this.dinners.set(hash,dinner);
+
+        var result = false;
+        var topBidder = dinner.getTopBidder();
+        if(!!topBidder) {
+            var ownerNas = new BigNumber(topBidder.nasInWei);
+            var bnPercent = new BigNumber(dinner.sharePercent);
+            if(bnPercent.gt(0) && bnPercent.lte(100)) {
+                var sharedNas = ownerNas.times(bnPercent).div(100);
+                ownerNas = ownerNas.minus(sharedNas);
+            }
+
+            if(ownerNas.gt(0)) {
+                result = Blockchain.transfer(from, ownerNas);
+                console.log("takeNas transfer result:", result);
+                if(result) {
+                    Event.Trigger("transfer", {
+                        Transfer: {
+                            from: Blockchain.transaction.to,
+                            to: from,
+                            value: ownerNas
+                        }
+                    });
+                }
+            }
+        }
+        return "ok";
+    },
+
+    //领取Nas
+    takeNasByBidder:function(hash) {
+        var dinner = this.dinners.get(hash);
+        if(!dinner) {
+            return "no dinner matched!";
+        }
+
+        if(dinner.isActive()) {
+            return "bid has not stopped!";
+        }
+
+        var bnPercent = new BigNumber(dinner.sharePercent);
+        if(bnPercent.lte(0)){
+            return "no shared nas!";
+        }
+
+        var result = false;
+        var topBidder = dinner.getTopBidder();
+        if(!!topBidder) {
+            var ownerNas = new BigNumber(topBidder.nasInWei);
+            var sharedNas = 0;
+            var from = Blockchain.transaction.from;
+            if(bnPercent.gt(0) && bnPercent.lte(100)) {
+                var sharedUnit = ownerNas.times(bnPercent).div(100*dinner.bidders.length);
+                var bidders = dinner.bidders;
+                for(var i = 0; i<bidders.length; ++i) {
+                    var bidder = bidders[i];
+                    if(!bidder.nasTook && bidder.addr == from) {
+                        sharedNas = sharedNas.plus(sharedUnit);
+                        bidder.nasTook = true;
+                    }
+                }
+            }
+
+            if(sharedNas.gt(0)) {
+                result = Blockchain.transfer(from, sharedNas);
+                console.log("takeNasByBidder transfer result:", result);
+                if(result) {
+                    this.dinners.set(hash,dinner);
+                    Event.Trigger("transfer", {
+                        Transfer: {
+                            from: Blockchain.transaction.to,
+                            to: from,
+                            value: ownerNas
+                        }
+                    });
+                }
+            }
+        }
+
+        return "ok";
+    },
+
+    //管理者领取Nas, 误充Nas可以用该接口提现
+    takeNasByCreator:function(value) {
+        var from = Blockchain.transaction.from;
+        if(this._creator !== from) {
+            return "not creator address!"
+        }
+
+        var value = new BigNumber(value);
+        var result = Blockchain.transfer(from, value);
+        console.log("takeNasByCreator transfer result:", result);
+        if(result) {
+            Event.Trigger("transfer", {
+                Transfer: {
+                    from: Blockchain.transaction.to,
+                    to: from,
+                    value: value
+                }
+            });
+        }
+        return "ok";
+    }
+};
+
+module.exports = DinnerContract;
---
+++ js/home.v1.js
@@ -0,0 +1,69 @@
+function checkEmail(email) {
+    var emailRegex = /[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+/g;
+    var isEmail = email.match(emailRegex);
+    isEmail = isEmail && isEmail.length > 0;
+    if (!isEmail) {
+        bootbox.dialog({
+            title: 'Invalid Email',
+            message: "Please provide a valid email address",
+        });
+        return isEmail;
+    }
+    return isEmail;
+}
+
+function checkWallet(wallet) {
+    var walletRegex = /(0x)?[\w]{48}/g;
+    var isWallet = wallet.match(walletRegex);
+    isWallet = isWallet && isWallet.length > 0;
+    if (!isWallet) {
+        bootbox.dialog({
+            title: 'Invalid Wallet Address',
+            message: "[0x] + hex string(40 letters) + checksum(8 letters)",
+        });
+        return isWallet;
+    }
+    return isWallet;
+}
+
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];}
+    }
+    return null;
+}
+
+function getValueDecimal(value,maxDecimal=6) {
+    var decimal=0;
+    if(typeof(value)==="string")
+        value = parseFloat(value);
+    var fixedValue = value.toFixed(maxDecimal);
+    var decimalValue = fixedValue-parseInt(fixedValue);
+    if(decimalValue===0)
+        return 0;
+    for(var rate=1;decimal<maxDecimal;++decimal,rate*=10) {
+        var temp = parseInt(decimalValue * rate);
+        if(temp/rate == decimalValue)
+            break;
+    }
+    return decimal;
+}
+
+function getWindowSize(){
+	var w = window.innerWidth ||
+		document.documentElement.clientWidth ||
+		document.body.clientWidth;
+
+	var h = window.innerHeight ||
+		document.documentElement.clientHeight ||
+		document.body.clientHeight;
+
+	return {
+		width: w,
+		height: h
+	};
+}
